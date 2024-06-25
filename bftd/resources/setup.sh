5990297e0ac1e1b492c7b6fec4ddbc92bdedbccc
andll <andll@danasoft.ws>
2024-06-25 21:35:11 +0200

    Add setup script

---
+++ bftd/resources/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -oe
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# todo source rust PATH
+sudo apt-get update
+sudo apt-get install build-essential prometheus iftop clang -y
+sudo mv ~/data/prometheus.yml /etc/prometheus/prometheus.yml
+sudo service prometheus reload
+cd ~/bftd && cargo build --release
