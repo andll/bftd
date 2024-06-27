4371a223dcefcb584871623ee94015226133fe98
andll <andll@danasoft.ws>
2024-06-27 17:52:28 +0200

    Do not report suspended blocks as newly missing from block manager

--- bftd-core/src/block_manager.rs
+++ bftd-core/src/block_manager.rs
@@ -2,19 +2,29 @@ use crate::block::{Block, BlockReference};
 use crate::metrics::Metrics;
 use crate::store::BlockStore;
 use std::collections::hash_map::Entry;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
 
 pub struct BlockManager<S> {
-    missing_inverse: HashMap<BlockReference, HashMap<BlockReference, Arc<Block>>>,
+    missing_inverse: HashMap<BlockReference, HashSet<BlockReference>>,
+    suspended: HashMap<BlockReference, SuspendedInfo>,
     store: S,
     metrics: Arc<Metrics>,
 }
 
+struct SuspendedInfo {
+    block: Arc<Block>,
+    missing_parents: HashSet<BlockReference>,
+}
+
 pub struct AddBlockResult {
+    /// Blocks that have all parents stored locally and are now stored in BlockStore
     pub added: Vec<Arc<Block>>,
+    /// Blocks that are received but suspended (do not have all parents)
     pub suspended: Vec<BlockReference>,
+    /// Missing parents references (was not seen before)
     pub new_missing: Vec<BlockReference>,
+    /// Missing parents references (either already suspended or referenced by some previously suspended block)
     pub previously_missing: Vec<BlockReference>,
 }
 
@@ -23,6 +33,7 @@ impl<S: BlockStore> BlockManager<S> {
         Self {
             store,
             missing_inverse: Default::default(),
+            suspended: Default::default(),
             metrics,
         }
     }
@@ -44,32 +55,62 @@ impl<S: BlockStore> BlockManager<S> {
                 .iter()
                 .filter_map(|p| if self.store.exists(p) { None } else { Some(*p) })
                 .collect();
+            let reference = *block.reference();
             if missing.is_empty() {
+                // Add block
                 let missing_inverse = self.missing_inverse.remove(block.reference());
                 self.metrics
                     .block_manager_missing_inverse_len
                     .set(self.missing_inverse.len() as i64);
                 if let Some(missing_inverse) = missing_inverse {
-                    // todo - need to be a bit smart about it to skip blocks that aren't ready yet
-                    for (k, v) in missing_inverse {
-                        blocks.insert(k, v);
+                    for k in missing_inverse {
+                        if let Entry::Occupied(mut oc) = self.suspended.entry(k) {
+                            let info = oc.get_mut();
+                            assert!(info.missing_parents.remove(block.reference()));
+                            if info.missing_parents.is_empty() {
+                                let block = oc.remove().block;
+                                assert_eq!(&k, block.reference());
+                                blocks.insert(k, block);
+                            }
+                        } else {
+                            panic!(
+                                "Block reference {k} is in missing_inverse, but not in suspended"
+                            );
+                        }
                     }
                 }
                 added.push(block.clone());
                 self.store.put(block);
             } else {
+                // Suspend block
+                match self.suspended.entry(reference) {
+                    Entry::Occupied(_oc) => {
+                        // nothing to do, already suspended this block
+                        continue;
+                    }
+                    Entry::Vacant(va) => {
+                        va.insert(SuspendedInfo {
+                            block,
+                            missing_parents: missing.iter().cloned().collect(),
+                        });
+                    }
+                }
                 // todo tests
                 // todo only newly suspended blocks
-                suspended.push(*block.reference());
+                suspended.push(reference);
                 for parent in missing {
                     let entry = self.missing_inverse.entry(parent);
                     if matches!(entry, Entry::Vacant(_)) {
-                        new_missing.push(parent);
+                        if self.suspended.contains_key(&parent) {
+                            previously_missing.push(parent);
+                        } else {
+                            new_missing.push(parent);
+                        }
                     } else {
                         previously_missing.push(parent);
                     }
                     let map = entry.or_default();
-                    map.insert(*block.reference(), block.clone());
+                    assert!(map.insert(reference));
                     self.metrics
                         .block_manager_missing_inverse_len
                         .set(self.missing_inverse.len() as i64);
@@ -113,8 +154,16 @@ mod tests {
         assert_eq!(block_store.store.lock().len(), 0);
         let r = block_store.add_block(blk(2, 2, vec![br(0, 0), br(1, 1)]));
         r.assert_added_empty();
-        r.assert_new_missing(vec![br(1, 1)]);
+        r.assert_no_new_missing();
+        r.assert_previously_missing(vec![br(1, 1), br(0, 0)]);
+
+        // Attempt to add same block again
+        let r = block_store.add_block(blk(2, 2, vec![br(0, 0), br(1, 1)]));
         assert_eq!(block_store.store.lock().len(), 0);
+        r.assert_added_empty();
+        r.assert_no_new_missing();
+        r.assert_no_previously_missing();
+
         let r = block_store.add_block(blk(1, 0, vec![]));
         r.assert_added_multi(vec![br(1, 0)]);
         r.assert_no_new_missing();
@@ -124,6 +173,7 @@ mod tests {
         r.assert_added_multi(vec![br(0, 0), br(1, 1), br(2, 2)]);
         r.assert_no_new_missing();
         assert_eq!(block_store.missing_inverse.len(), 0);
+        assert_eq!(block_store.suspended.len(), 0);
         assert_eq!(
             block_store.metrics.block_manager_missing_inverse_len.get(),
             0
@@ -190,6 +240,11 @@ mod tests {
         }
 
         #[track_caller]
+        pub fn assert_no_previously_missing(&self) {
+            assert!(self.previously_missing.is_empty())
+        }
+
+        #[track_caller]
         pub fn assert_added_multi(&self, v: Vec<BlockReference>) {
             assert_eq!(
                 HashSet::<BlockReference>::from_iter(self.added.iter().map(|b| *b.reference())),
@@ -201,6 +256,14 @@ mod tests {
         pub fn assert_new_missing(&self, v: Vec<BlockReference>) {
             assert_eq!(
                 HashSet::<BlockReference>::from_iter(self.new_missing.iter().cloned()),
+                HashSet::from_iter(v.into_iter())
+            )
+        }
+
+        #[track_caller]
+        pub fn assert_previously_missing(&self, v: Vec<BlockReference>) {
+            assert_eq!(
+                HashSet::<BlockReference>::from_iter(self.previously_missing.iter().cloned()),
                 HashSet::from_iter(v.into_iter())
             )
         }
