8c11ac328750f31e3776407ee079abf13000d137
andll <andll@danasoft.ws>
2024-07-30 17:17:51 +0100

    Removed iftop from setup script

--- bftd-server/resources/setup.sh
+++ bftd-server/resources/setup.sh
@@ -3,7 +3,7 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
 sudo apt-get update
-sudo apt-get install build-essential prometheus iftop clang -y
+sudo apt-get install build-essential prometheus clang -y
 sudo mv ~/data/prometheus.yml /etc/prometheus/prometheus.yml
 sudo service prometheus reload
 cd ~/bftd && cargo build --release
